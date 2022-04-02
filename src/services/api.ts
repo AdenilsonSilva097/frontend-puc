208a38466ec366b8616f91be3e455d03d52bff19
Adenilson A. da Silva <adenilson@topsys.com>
2022-04-02 17:09:24 -0300

    add base url for api production

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.REACT_APP_SERVER_TEST === "ON"
     ? "http://192.168.0.110:3001"
-    : "insert your server url here"
+    : process.env.REACT_APP_BASE_URL_API
 });
 
 export default api;
